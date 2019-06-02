f26bf6e390ee7ea381ad65f471a0441443e930c2
gbs4ever <gbs4ever2@gmail.com>
2019-06-02 01:50:56 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
 // Code MovieReviews Here
+const MovieReviews = ({reviews}) =>  {
+return(
+  <div className= "review-list">
+{reviews.map(review =>{
+  return (
+
+    <div className="review">
+  <li>{review.display_title}</li>
+      <li>{review.summary_short}</li>
+      <li>{review.opening_date}</li>
+  </div>
+    )
+})}
+    </div>)
+}
+
+
+
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,7 +3,56 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?query'
+  + `api-key=${NYT_API_KEY} & query=`;
 
 // Code SearchableMovieReviewsContainer Here
+export  class SearchableMovieReviewsContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+     reviews: "",
+     searchTerm: ""
+    };
+  }
+
+  handleChange = (event)=>{
+    this.setState(
+      {searchTerm: event.target.value }
+
+    )
+  }
+  onsubmit = () => {
+    fetch(`${URL}`+`${this.state.searchTerm}`)
+      .then(response => response.json())
+      .then(data => this.setState({ reviews: data.results }))
+  }
+
+
+  
+
+
+
+
+
+
+
+
+  render() {
+  return (
+  <div className= "searchable-movie-reviews">
+      <input type="text" value={this.state.searchTerm} onChange={this.handleChange}></input> <br></br>
+      <button onClick={this.onsubmit} >Search movie reviews  </button>
+    <MovieReviews  reviewSearch={this.state.reviews}/>
+  </div>
+
+)
+
+  }
+
+
+
+
+}
